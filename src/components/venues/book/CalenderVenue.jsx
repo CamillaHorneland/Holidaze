411b78995cdf406ee0d37f6c16c7c664a2388577
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2024-02-11 00:48:32 +0100

    Booking of venue complete

--- src/components/venues/book/CalenderVenue.jsx
+++ src/components/venues/book/CalenderVenue.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { ALLVENUES_URL } from '../../../constant/api';
 import { useQuery } from '@tanstack/react-query';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import nb from 'date-fns/locale/nb';
 import ConfirmationModal from './ConfirmationModal';
+import { useUser } from '../../type/UserContext';
 
 async function getBookingDetail(venueId) {
   try {
     const response = await fetch(`${ALLVENUES_URL}/${venueId}?_bookings=true`);
 
     if (!response.ok) {
-      throw new Error('There was an error fetching booking details');
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
     const data = await response.json();
@@ -29,105 +30,114 @@ const BookingDetail = ({ venueId }) => {
   const [guests, setGuests] = useState(1);
   const [excludedDates, setExcludedDates] = useState([]);
   const [isModalOpen, setModalOpen] = useState(false);
-
+  const { user } = useUser();
+ 
   const onChange = (dates) => {
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
   };
 
-   const { data: bookingData, isLoading: isBookingLoading, isError: isBookingError } = useQuery({
+  const { data: bookingData, isLoading: isBookingLoading, isError: isBookingError } = useQuery({
     queryKey: ['booking', venueId],
     queryFn: () => getBookingDetail(venueId),
     staleTime: 1000 * 60 * 5,
   });
 
   useEffect(() => {
+    if (bookingData && bookingData.bookings) {
+      const newExcludedDates = bookingData.bookings.map((booking) => ({
+        startDate: new Date(booking.dateFrom),
+        endDate: new Date(booking.dateTo),
+      }));
 
-  if (bookingData && bookingData.bookings) {
-    const newExcludedDates = bookingData.bookings.map((booking) => ({
-      startDate: new Date(booking.dateFrom),
-      endDate: new Date(booking.dateTo),
-    }));
+      const flattenedExcludedDates = newExcludedDates.flatMap(({ startDate, endDate }) => {
+        const dates = [];
+        let currentDate = new Date(startDate);
 
-    const flattenedExcludedDates = newExcludedDates.flatMap(({ startDate, endDate }) => {
-      const dates = [];
-      let currentDate = new Date(startDate);
+        while (currentDate <= endDate) {
+          dates.push(new Date(currentDate));
+          currentDate.setDate(currentDate.getDate() + 1);
+        }
 
-      while (currentDate <= endDate) {
-        dates.push(new Date(currentDate));
-        currentDate.setDate(currentDate.getDate() + 1);
-      }
+        return dates;
+      });
 
-      return dates;
-    });
-
-    setExcludedDates(flattenedExcludedDates);
-  }
-}, [bookingData]);
+      setExcludedDates(flattenedExcludedDates);
+    }
+  }, [bookingData]);
 
   return (
     <>
-      {bookingData && (
+      {user && user.role === 'guest' ? (
         <div className="bg-blue m-10 pb-20">
           <h3 className="text-lg font-bold p-5">Book this venue:</h3>
-          <div className="flex flex-wrap p-4 space-between justify-center">
-            <div className="mb-4 sm:mb-0">
-              <DatePicker
-                id="startDate"
-                dateFormat="dd/MM/yyyy"
-                selected={startDate}
-                onChange={onChange}
-                startDate={startDate}
-                endDate={endDate}
-                excludeDates={excludedDates}
-                selectsRange
-                monthsShown={2}
-                selectsDisabledDaysInRange
-                 minDate={new Date()}
-                inline
-                locale={nb}
-              />
-            </div>
-            <div className="mt-5 ml-10">
-              <label htmlFor="guests" className="block text-gray-700">
-                Guests:
-              </label>
-              <input
-                type="number"
-                id="guests"
-                value={guests}
-                onChange={(e) => setGuests(e.target.value)}
-                min={1}
-                max={bookingData.maxGuests}
-                className="align-center p-2 block border border-gray-300 rounded-md"
-              />
-              <p className="text-light-blue m-4">(Max guest for this venue {bookingData.maxGuests})</p>
-              <button
-              className="hover:bg-light-blue bg-blue border-2 border-light-blue m-2 text-black font-bold py-2 p-4 rounded-full w-full"
-              onClick={() => setModalOpen(true)} 
-            >
-              Go to booking
-            </button>
+          <p className="text-white m-4">You have to login to book a venue. <NavLink to="/login" className="text-white font-bold ml-5">Login here.</NavLink></p>
+        </div>
+      ) : (
+        bookingData && (
+          <div className="bg-blue m-10 pb-20">
+            <h3 className="text-lg font-bold p-5">Book this venue:</h3>
+            <div className="flex flex-wrap p-4 space-between justify-center">
+              <div className="mb-4 sm:mb-0">
+                <DatePicker
+                  id="startDate"
+                  dateFormat="dd/MM/yyyy"
+                  selected={startDate}
+                  onChange={onChange}
+                  startDate={startDate}
+                  endDate={endDate}
+                  excludeDates={excludedDates}
+                  selectsRange
+                  monthsShown={2}
+                  selectsDisabledDaysInRange
+                  minDate={new Date()}
+                  inline
+                  locale={nb}
+                />
+              </div>
+              <div className="mt-5 ml-10">
+                <label htmlFor="guests" className="block text-gray-700">
+                  Guests:
+                </label>
+                <input
+                  type="number"
+                  id="guests"
+                  value={guests}
+                  onChange={(e) => setGuests(e.target.value)}
+                  min={1}
+                  max={bookingData.maxGuests}
+                  className="align-center p-2 block border border-gray-300 rounded-md"
+                />
+                <p className="text-light-blue m-4">(Max guest for this venue {bookingData.maxGuests})</p>
+                <button
+                  className="hover:bg-light-blue bg-blue border-2 border-light-blue m-2 text-black font-bold py-2 p-4 rounded-full w-full"
+                  onClick={() => setModalOpen(true)}
+                >
+                  Go to booking
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        )
       )}
 
-       {isModalOpen && (
-      <ConfirmationModal
-        startDate={startDate}
-        endDate={endDate}
-        guests={guests}
-        venueId={venueId}
-        onClose={() => setModalOpen(false)} 
-      />
-       )}
+      {isModalOpen && (
+        <ConfirmationModal
+          startDate={startDate}
+          endDate={endDate}
+          guests={guests}
+          venueId={venueId}
+          onClose={() => setModalOpen(false)}
+          price={bookingData.price}
+        />
+      )}
     </>
   );
 };
 
 export default BookingDetail;
+
 
 
 
--- src/components/venues/book/ConfirmationModal.jsx
+++ src/components/venues/book/ConfirmationModal.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { format, differenceInDays } from 'date-fns';
 import { NEWBOOKING_URL } from '../../../constant/api';
 import { useUser } from '../../type/UserContext';
+import { NavLink } from 'react-router-dom';
 
 async function createBooking({ dateFrom, dateTo, guests, venueId, user }) {
   try {
@@ -33,11 +35,17 @@ async function createBooking({ dateFrom, dateTo, guests, venueId, user }) {
   }
 }
 
-function ConfirmationModal({ startDate, endDate, guests, venueId, onClose }) {
+function ConfirmationModal({ startDate, endDate, guests, venueId, onClose, price  }) {
   const [isConfirming, setConfirming] = useState(false);
   const [isBookingComplete, setBookingComplete] = useState(false);
+  const [bookingErrorMessage, setBookingErrorMessage] = useState('');
 
   const { user } = useUser();
+
+  const formattedStartDate = format(new Date(startDate), 'EEEE dd.MM.yyyy');
+  const formattedEndDate = format(new Date(endDate), 'EEEE dd.MM.yyyy');
+  const numberOfNights = differenceInDays(new Date(endDate), new Date(startDate));
+  const totalCost = price * numberOfNights;
 
   const handleBookingConfirm = async () => {
     try {
@@ -57,48 +65,80 @@ function ConfirmationModal({ startDate, endDate, guests, venueId, onClose }) {
 
       setBookingComplete(true);
     } catch (error) {
-     
+      console.error('Error confirming booking:', error);
+
+      if (error.message.includes('Booking failed')) {
+        setBookingErrorMessage('Problem completing this booking. Try again or choose different dates.');
+      } else {
+        setBookingErrorMessage('An error occurred while confirming the booking. Please try again.');
+      }
     } finally {
       setConfirming(false);
     }
   };
-
+  
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-gray-500 bg-opacity-75 flex items-center justify-center">
       <div className="bg-white p-6 rounded-lg w-96">
-        <h3 className="text-lg font-bold mb-4">Booking Confirmation</h3>
-        <p>Date: {startDate.toString()} to {endDate.toString()}</p>
-        <p>Guests: {guests}</p>
+        <h2 className="text-lg text-dark-blue font-bold mb-4">Complete Booking</h2>
+        <h3 className='font-bold mb-4'>Selected booking dates:</h3>
+        <p className='mb-2'>From: {formattedStartDate}</p> 
+        <p className='mb-2'>To: {formattedEndDate}</p>
+        <p className='mb-2'>Guests: {guests}</p>
+        <p className='mb-2'>Number of nights: {numberOfNights}</p>
+        <p className='font-bold'>Total cost: {totalCost} EUR</p>
         {isBookingComplete && (
-          <p className="text-green-500 mt-4">Booking complete!</p>
+          <h3 className="text-dark-blue mt-4">Booking complete!</h3>
+        )}
+        {bookingErrorMessage && (
+          <p className="text-red-500 mt-4">{bookingErrorMessage}</p>
         )}
         <div className="mt-6">
           {!isBookingComplete ? (
-            <button
-              onClick={handleBookingConfirm}
-              disabled={isConfirming}
-              className={`bg-blue text-white py-2 px-4 rounded ${
-                isConfirming ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
-            >
-              {isConfirming ? 'Confirming...' : 'Confirm Booking'}
-            </button>
+            <>
+              <button
+                onClick={handleBookingConfirm}
+                disabled={isConfirming}
+                className={`bg-blue hover:bg-dark-blue text-white py-2 px-4 rounded ${
+                  isConfirming ? 'opacity-50 cursor-not-allowed' : ''
+                }`}
+              >
+                {isConfirming ? 'Confirming...' : 'Confirm Booking'}
+              </button>
+              <button
+                onClick={onClose}
+                className="ml-2 bg-blue hover:bg-dark-blue text-white py-2 px-4 rounded"
+              >
+                Close
+              </button>
+            </>
           ) : (
-            <button
-              onClick={onClose}
-              disabled={isConfirming}
-              className="bg-blue text-white py-2 px-4 rounded"
-            >
-              Close
-            </button>
+            <>
+              <button
+                onClick={onClose}
+                disabled={isConfirming}
+                className="bg-blue hover:bg-dark-blue text-white py-2 px-4 rounded"
+              >
+                Close
+              </button>
+              <NavLink to="/bookings">
+                <button
+                  className="ml-2 bg-blue hover:bg-dark-blue text-white py-2 px-4 rounded"
+                >
+                  Go to Bookings
+                </button>
+              </NavLink>
+            </>
           )}
         </div>
       </div>
     </div>
-  );
+ );
 }
 
 export default ConfirmationModal;
+
+
 
 
 
